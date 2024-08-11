59e16170a2ab01f57bbf679bf02eb20af6cbef92
Andres Salazar <92761344+AndresSalazarAlturo@users.noreply.github.com>
2024-08-12 00:48:54 +0100

    [UPDATE] Tournament selection

--- utils/my_GA_cam_based.py
+++ utils/my_GA_cam_based.py
@@ -3,13 +3,20 @@ import random
 import numpy as np
 
 MUTATION_PROBABILITY = 0.1
-FIXED_BIAS = False			## False is better 
-BOUNDS = 5
-ELITE_PART = 0.4
+BOUNDS = [0.01, 0.99]
+#~ BOUNDS = [-1, 1]
+range_factor = 0.3
+tournament_size = 2
+num_tournaments = 45
+
+## For testing
+#~ random.seed(27)
 
 def create_random_parameters_set(pop_size, geno_size, weights_bias_range):
     """
         Create random parameters set.
+        :param pop_size: Population size
+        :param geno_size: Genotype size
         :param weights_bias_range: Range with the possible values
         :return population: List of list with the genotypes
     """
@@ -17,23 +24,19 @@ def create_random_parameters_set(pop_size, geno_size, weights_bias_range):
     for pop_ind in range(pop_size):
         genotype = [0] * geno_size
         for ind in range(len(genotype)):
-            if FIXED_BIAS:
-                genotype[ind] = random.choice(weights_bias_range)
-                genotype[120] = 5
-                genotype[121] = 5
-            else:
-                genotype[ind] = random.choice(weights_bias_range)
+            genotype[ind] = random.choice(weights_bias_range)
         population[pop_ind] = genotype
     return population
 
-def create_random_single_parameters_set(weights_bias_range):
+def create_random_single_parameters_set(weights_bias_range, geno_size):
     """
-        Create random parameters set.
+        Create random parameter set.
         :param weights_bias_range: Range with the possible values
-        :return population: List of list with the genotypes
+        :param geno_size: Genotype size
+        :return genotype: List of list with the genotypes
     """
 
-    genotype = [0] * 122
+    genotype = [0] * geno_size
     for ind in range(len(genotype)):
         genotype[ind] = random.choice(weights_bias_range)
 
@@ -61,85 +64,76 @@ def random_number_close_range(current_value, x, bounds, int_value = False):
 
     else:
         ## Generate random rational number between lower and upper bounds
-        close_range = np.arange(lower_bound, upper_bound, 0.5)
-        # print("Close range value: ", close_range)
+        close_range = np.arange(lower_bound, upper_bound, 0.01)
+        #~ print("Close range value: ", close_range)
         random_rational_number = random.choice(close_range)
-        while random_rational_number < -bounds or random_rational_number > bounds:
+        while random_rational_number < bounds[0] or random_rational_number > bounds[1]:
             random_rational_number = random.choice(close_range)
+        #~ print(f"Random rational number: {random_rational_number}")
 
     return random_rational_number
 
-#~ def population_reproduce(p,fitness, n_genes):
+def tournament_selection(pop_data):
+    """
+        Perform tournament selection.
+        :param pop_data: Population and fitness data - list
+        :param tournament_size: Size of the tournament - int
+        :return: Winner and loser genotypes
+    """
 
-    #~ pop_size = len(p)
-    #~ new_p = []
+    candidate1, candidate2 = random.sample(pop_data, tournament_size)
 
-    #~ dataframe = pd.DataFrame({"Param":p,"Fitness":fitness})
-    #~ dataframe = dataframe.sort_values(['Fitness'], ascending=False)
-    #~ dataframe = dataframe.reset_index(drop=True)
+    if candidate1['fitness'] > candidate2['fitness']:
+        winner = candidate1
+        loser = candidate2
+    else:
+        winner = candidate2
+        loser = candidate1
 
-    #~ print("Data frame: ", dataframe)
+    return winner, loser
 
-    #~ # print("input population: ", p)
-
-    #~ sorted_p = dataframe['Param'].tolist()
-
-    #~ # print("Sorted parameters: ", sorted_p)
-
-    #~ ## Selection
-    #~ elite_part = round(ELITE_PART * pop_size)
-    #~ new_p = new_p + sorted_p[:elite_part]
-
-    #~ for i in range(pop_size-elite_part):
-        #~ mom = p[random.randint(0, pop_size - 1)]
-        #~ dad = p[random.randint(0, pop_size - 1)]
-        #~ child = crossover(mom, dad, n_genes, p)
-        #~ child = mutate(child, n_genes, FIXED_BIAS)
-        #~ new_p.append(child)
-
-    #~ return new_p
-    
 def population_reproduce(p,fitness, n_genes):
 
     pop_size = len(p)
     new_p = []
 
-    dataframe = pd.DataFrame({"Param":p,"Fitness":fitness})
-    dataframe = dataframe.sort_values(['Fitness'], ascending=True)
-    dataframe = dataframe.reset_index(drop=True)
+    ## Create the population data
+    pop_data = [0] * pop_size
+    for cand in range(pop_size):
+        temp_dict = {"genome":p[cand], "fitness":fitness[cand]}
+        pop_data[cand] = temp_dict
 
-    print("Data frame: ", dataframe)
+    ## Tournament selection
+    for _ in range(num_tournaments):
 
-    # print("input population: ", p)
+        winner, loser = tournament_selection(pop_data)
+        new_p.append(winner['genome'])
 
-    sorted_p = dataframe['Param'].tolist()
+    for _ in range(pop_size-num_tournaments):
 
-    # print("Sorted parameters: ", sorted_p)
-
-    ## Selection
-    elite_part = round(ELITE_PART * pop_size)
-    new_p = new_p + sorted_p[:elite_part]
-
-    for i in range(pop_size-elite_part):
         mom = p[random.randint(0, pop_size - 1)]
         dad = p[random.randint(0, pop_size - 1)]
-        child = crossover(mom, dad, n_genes, p)
-        child = mutate(child, n_genes, FIXED_BIAS)
+
+        ## Check that mom and dad are not the same individual
+        if mom == dad:
+            while mom == dad:
+                mom = p[random.randint(0, pop_size - 1)]
+                dad = p[random.randint(0, pop_size - 1)]
+
+        child = crossover(mom, dad)
+        child = mutate(child, n_genes)
         new_p.append(child)
 
     return new_p
 
 def population_get_fittest(p,f):
-    
-    # print("fitness", f)
-    # print("population", p)
 
     f = np.array(f)
     p = np.array(p)
 
-    pop_best_fitness = min(f)
+    pop_best_fitness = max(f)
     # print("max fitness", pop_best_fitness)
-    pop_best_fitness_pos = np.argmin(f)
+    pop_best_fitness_pos = np.argmax(f)
 
     # print("pos best fitness position: ", pop_best_fitness_pos)
 
@@ -151,49 +145,31 @@ def population_get_fittest(p,f):
 def population_get_average_fitness(f):
     """
         Get population average fitness.
-        :para f: List with population fitness.
+        :param f: List with population fitness.
     """
     return sum(f)/len(f)
 
-def crossover(p1,p2, n_genes, pop):
+def crossover(p1,p2):
 
     crossover = []
     locii = [random.randint(0,8) for _ in range(len(p1))]
-    
+
     for i in range(len(p1)):
         if locii[i]>4:
             crossover.append(p2[i])
         else:
             crossover.append(p1[i])
 
-    if FIXED_BIAS:
-        crossover[120] = 5
-        crossover[121] = 5
-
     return crossover
 
-def mutate(child, n_genes, FIXED_BIAS):
+def mutate(child, n_genes):
     """
         Implement mutation.
     """
+    for gene_no in range(n_genes):
+        if np.random.rand() < MUTATION_PROBABILITY:
+            child[gene_no] = random_number_close_range(child[gene_no], range_factor, BOUNDS)
 
-    if FIXED_BIAS:
-
-        ## Set bias values to 2
-        child[120] = 5
-        child[121] = 5
-
-        for gene_no in range(n_genes):
-            if np.random.rand() < MUTATION_PROBABILITY:
-                ## Gene 120 is bias for left motor
-                ## Gene 121 is bias for right motor
-                if gene_no == 120 or gene_no == 121:
-                    child[gene_no] = child[gene_no]
-                else:
-                    child[gene_no] = random_number_close_range(child[gene_no], 1, BOUNDS)
-    else:
-        for gene_no in range(n_genes):
-            if np.random.rand() < MUTATION_PROBABILITY:
-                child[gene_no] = random_number_close_range(child[gene_no], 1, BOUNDS)
+    #~ print(f"I am mutating")
 
     return child
